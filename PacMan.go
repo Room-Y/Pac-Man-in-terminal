fab512558791e2aa2b1c51c5956a9dce123b8046
cmr <1018419943@qq.com>
2022-11-22 21:13:10 +0800

    step 02

--- PacMan.go
+++ PacMan.go
@@ -5,15 +5,20 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
+
+	"github.com/danicat/simpleansi"
 )
 
 var maze []string
 
 func main() {
 	// initialize game
+	initialise() //启用stty的 cbreak 模式关闭 echo
+	defer cleanup()
 
 	// load resources
-	err := loadMaze("maze0.txt")
+	err := loadMaze("maze01.txt") //载入地图信息
 	if err != nil {
 		log.Println("failed to load maze:", err)
 		return
@@ -25,6 +30,15 @@ func main() {
 		printScreen()
 
 		// process input
+		input, err := readInput()
+		if err != nil {
+			log.Print("error reading input:", err)
+			break
+		}
+
+		if input == "ESC" {
+			break
+		}
 
 		// process movement
 
@@ -55,7 +69,43 @@ func loadMaze(file string) error {
 }
 
 func printScreen() {
+	simpleansi.ClearScreen()
 	for _, line := range maze {
 		fmt.Println(line)
 	}
+}
+
+func initialise() {
+	cbTerm := exec.Command("stty", "cbreak", "-echo")
+	cbTerm.Stdin = os.Stdin
+
+	err := cbTerm.Run()
+	if err != nil {
+		log.Fatalln("unable to activate cbreak mode:", err)
+	}
+}
+
+func cleanup() {
+	cookedTerm := exec.Command("stty", "-cbreak", "echo")
+	cookedTerm.Stdin = os.Stdin
+
+	err := cookedTerm.Run()
+	if err != nil {
+		log.Fatalln("unable to restore cbreak mode:", err)
+	}
+}
+
+func readInput() (string, error) {
+	buffer := make([]byte, 100)
+
+	cnt, err := os.Stdin.Read(buffer)
+	if err != nil {
+		return "", err
+	}
+
+	if cnt == 1 && buffer[0] == 0x1b {
+		return "ESC", nil
+	}
+
+	return "", nil
 }
